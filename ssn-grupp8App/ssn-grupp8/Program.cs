65df694a165bee8ba9a7c6eec868126b2121f380
Robert Vu <vurobert.rv@gmail.com>
2025-01-13 09:11:23 +0100

    add vs

---
+++ ssn-grupp8App/ssn-grupp8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ssn_grupp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
