500f9ce81dc7a5f25df39fbed35292b7718e9161
Robert Vu <vurobert.rv@gmail.com>
2025-01-13 10:56:43 +0100

    Add SSNCode & .gitignore

--- ssn-grupp8App/ssn-grupp8/Program.cs
+++ ssn-grupp8App/ssn-grupp8/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string numb = "8907163570";
+
+            if (string.IsNullOrWhiteSpace(numb) || numb.Length != 10 || !numb.All(char.IsDigit))
+            {
+                Console.WriteLine("Ogitligt format, endast number och 10 siffro");
+            }
+            else
+            {
+                Console.WriteLine("Korrekt inmatning");
+            }
+
+            int[] intnumb = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                intnumb[i] = numb[i] - '0';
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                int value = intnumb[i];
+                if (i % 2 == 0)
+                {
+                    value *= 2;
+                    if (value > 9)
+                    {
+                        value -= 9;
+                    }
+                }
+                sum += value;
+            }
+            int ssnControllDigit = (10 -(sum % 10)) % 10;
+
+            if (ssnControllDigit == intnumb[9])
+            {
+                Console.WriteLine($"Personnummret är gigligt, kontrollsiffra: {ssnControllDigit}");
+            }
+            else
+            {
+                Console.WriteLine($"Personnumret ör ogiligt! Kontrollsiffra borde vara {ssnControllDigit}, men är {intnumb[9]}.");
+            }
+
+
         }
     }
 }
