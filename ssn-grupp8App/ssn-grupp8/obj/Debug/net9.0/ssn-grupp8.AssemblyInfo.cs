0732c583fed7fba1e3200af4fcbb098eb06a0ff5
Robert Vu <vurobert.rv@gmail.com>
2025-01-13 11:58:48 +0100

    Flyttar runt mappar

--- ssn-grupp8App/ssn-grupp8/obj/Debug/net9.0/ssn-grupp8.AssemblyInfo.cs
+++ ssn-grupp8App/ssn-grupp8/obj/Debug/net9.0/ssn-grupp8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ssn-grupp8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5ec5b3d05f290b3f17034d78e086eb28fe431e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7de0479fdae6dd4a9629a4c397ea1eb45fb5bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ssn-grupp8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ssn-grupp8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ssn-grupp8App/ssn-grupp8/obj/Release/net9.0/ssn-grupp8.AssemblyInfo.cs
+++ ssn-grupp8App/ssn-grupp8/obj/Release/net9.0/ssn-grupp8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ssn-grupp8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e60c5f5c42a51470a5b0002866e618eb53609d45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7de0479fdae6dd4a9629a4c397ea1eb45fb5bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ssn-grupp8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ssn-grupp8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
